66ac13d432f071c5b13a0d37cdf1c8d03b78a50d
Ike <hayesik@whs.school.nz>
2021-05-07 14:57:58 +1200

    making game in text

---
+++ Game.java
@@ -0,0 +1,21 @@
+/**
+ * Write a description of class Game here.
+ *
+ * @author Ike Hayes
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class Game
+{
+    // instance variables - replace the example below with your own
+    int score;
+    /**
+     * Constructor for objects of class Game
+     */
+    public Game()
+    {
+        // initialise instance variables
+        
+    }
+
+}
