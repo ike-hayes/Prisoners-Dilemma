293a1e2584561bf8fb785fdc88a767a05d5f9729
Ike <hayesik@whs.school.nz>
2021-05-07 12:43:36 +1200

    Initial sharing of project

---
+++ Player.java
@@ -0,0 +1,18 @@
+/**
+ * Write a description of class Player here.
+ *
+ * @author Ike Hayes
+ * @version 7/5/21
+ */
+public class Player
+{
+    // instance variables - replace the example below with your own
+    /**
+     * Constructor for objects of class Player
+     */
+    public Player()
+    {
+        // initialise instance variables
+        
+    }
+}
